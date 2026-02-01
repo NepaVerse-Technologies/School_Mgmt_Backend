46e7460252695c8d6020ba496a99df6481cf6b49
rijanniraula <rijann38@gmail.com>
2026-02-01 22:02:22 +0545

    minor

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/index.js";
+import errorHandler from "./middleware/errorHandler.js";
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+
+app.use(errorHandler);
 
 app.use("/api", router);
 
