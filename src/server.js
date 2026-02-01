1fb37630f1f8d70a9883fc0e343b2bf1fec08930
rijanniraula <rijann38@gmail.com>
2026-02-01 21:17:11 +0545

    project setup

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes/index.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api", router);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
